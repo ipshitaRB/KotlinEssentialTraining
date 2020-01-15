f9dfb578bc8e810c6a605c4a33a4c2d6cb77911b
ipshitaRB <ipshitarburman@gmail.com>
2020-01-15 16:47:42 -0500

    Print command line argument with String templates

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,4 @@
 fun main(args: Array<String>) {
-    println("Welcome to Kotlin Essential Training!")
+    println("Welcome to Kotlin Essential Training, ${args[0]}!")// string templates
+    println("Hello " + args[0])
 }
